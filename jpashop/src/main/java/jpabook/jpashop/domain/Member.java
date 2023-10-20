858bfd60958dc6d63e456df5957f50e39d08a7c3
IMWoo94 <leemeawoo94@gmail.com>
2023-10-20 21:29:14 +0900

    web part2 create member api develop 2023.10.20

--- jpashop/src/main/java/jpabook/jpashop/domain/Member.java
+++ jpashop/src/main/java/jpabook/jpashop/domain/Member.java
@@ -2,14 +2,13 @@ package jpabook.jpashop.domain;
 
 import java.util.ArrayList;
 import java.util.List;
-
 import javax.persistence.Column;
 import javax.persistence.Embedded;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
-
+import javax.validation.constraints.NotEmpty;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -18,17 +17,18 @@ import lombok.Setter;
 @Setter
 public class Member {
 
-	@Id
-	@GeneratedValue
-	@Column(name = "member_id")
-	private Long id;
+    @Id
+    @GeneratedValue
+    @Column(name = "member_id")
+    private Long id;
 
-	private String username;
+    @NotEmpty
+    private String username;
 
-	@Embedded
-	private Address address;
-	
-	@OneToMany(mappedBy = "member")
-	private List<Order> orders = new ArrayList<>();
+    @Embedded
+    private Address address;
+
+    @OneToMany(mappedBy = "member")
+    private List<Order> orders = new ArrayList<>();
 
 }
